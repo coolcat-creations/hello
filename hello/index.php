b4b1554a87088c8eaa3ab6536b6818c170046419
coolcat-creations <mail@coolcat-creations.com>
2020-02-17 16:57:59 +0100

    Initial commit
    Initial commit

---
+++ hello/index.php
@@ -0,0 +1,43 @@
+<?php
+defined('_JEXEC') or die;
+
+JHtml::_('stylesheet', 'template.css', array('version' => 'auto', 'relative' => true));
+
+$app = JFactory::getApplication();
+$params = $app->getTemplate(true)->params;
+$logo = $params->get('logoBild');
+$logotext = $params->get('logoText');
+
+?>
+
+<!DOCTYPE html>
+<html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<jdoc:include type="head" />
+</head>
+<body>
+<div class="container">
+	<jdoc:include type="modules" name="menu" style="xhtml"/>
+
+	<?php if($logo || $logotext) : ?>
+		<a href="#" class="logo">
+			<?php if($logo) : ?>
+				<img src="<?php echo $logo; ?>" alt="logo">
+			<?php else : ?>
+				<?php echo $logotext; ?>
+			<?php endif;?>
+		</a>
+	<?php endif; ?>
+
+
+	<main>
+		<jdoc:include type="component" />
+	</main>
+
+	<jdoc:include type="modules" name="aside" style="xhtml"/>
+	<jdoc:include type="modules" name="footer" style="xhtml"/>
+
+</div>
+</body>
+</html>
